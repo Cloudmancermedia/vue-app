2ac7f6071a60c245d93be30f039d36c77f375e1d
Ryan Williams <52295078+Cloudmancermedia@users.noreply.github.com>
2024-06-19 15:34:22 -0600

    adding branch specific messaging

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="This is the dev branch" />
+      <HelloWorld msg="This is the dev branch!!!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
